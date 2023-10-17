1610e36b7ff67acaee3fb08458763fcee577fcb3
Michael Gingras <mcg79@cornell.edu>
2023-10-17 15:52:59 -0600

    Memory

--- README.md
+++ README.md
@@ -17,3 +17,6 @@ npm run test
 
 Todo:
 Performance is starting to be an issue, and I haven't even worked on the ALU yet. Maybe a CLA (carry look-ahead adder) would help improve efficiency here?
+
+Notes:
+Something I'm thinking about with this project is how I learned much of this in 3420 at Cornell. Yet, I don't actually remember much aside from the high level details. I'm sure at some point I'll forget what I learned while implementing this project, but it would be cool if somehow this project was self annotating so I could come back and quickly jot my memory. Why is that so difficult?
---
+++ src/modules/memory/dff.ts
@@ -0,0 +1,34 @@
+// import { createNandGate } from "../gates/nand-gate";
+// import { createNotGate } from "../gates/not-gate";
+
+/**
+ * @param input
+ * @param clock
+ * @param label
+ * @returns output, output'
+ *
+ * Data Flip Flop
+ */
+
+export const createDFF = (
+  input: HTMLInputElement,
+  clk: HTMLInputElement,
+  label?: string
+) => {
+  // TODO: replace with real DFF (if possible?)
+  dffCheat(input, clk, label);
+};
+
+// I'm having a hard time implementing the DFF out of my gates because the input relies on the output in a cyclical way that is hard to model. So, this "cheat" DFF won't actually be built out of NAND gates but will just be a passthrough layer that takes inputs and outputs and follows the DFF interface.
+const dffCheat = (
+  input: HTMLInputElement,
+  clk: HTMLInputElement,
+  label?: string
+) => {
+  // todo
+};
+
+// const [q, qp] = createDFF(input, clk, label);
+// api to handle this programatically
+// clk.click -> toggles clock
+// input.click -> toggles input

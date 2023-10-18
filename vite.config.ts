530aca7564baded711aaf7bab50ab2a838d43240
Michael Gingras <mcg79@cornell.edu>
2023-10-17 21:49:44 -0600

    Adds new routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vitest/config";
+import { resolve } from "path";
 
 export default defineConfig({
   test: {
     environment: "jsdom",
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        and: resolve(__dirname, "gates/and.html"),
+        nand: resolve(__dirname, "gates/nand.html"),
+      },
+    },
   },
 });
